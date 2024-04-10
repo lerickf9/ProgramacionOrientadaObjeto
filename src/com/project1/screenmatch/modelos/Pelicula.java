f9ffa5c2c97c0525b0ba022bce3108e037de5914
Erick <efuentes.edf9@gmail.com>
2024-04-10 00:10:08 -0400

    Clase Serie-Titulo more extends

--- src/com/project1/screenmatch/modelos/Pelicula.java
+++ src/com/project1/screenmatch/modelos/Pelicula.java
@@ -1,63 +1,14 @@
 package com.project1.screenmatch.modelos;
 
-public class Pelicula {
-    private String nombre;
+public class Pelicula extends Titulo{
 
-    private int fechaLanzamiento;
-    private int duracionEnMinuto;
-    private boolean incluidoEnPlan;
+    private String director;
 
-    private int totalEvaluaciones;
-    private double sumaEvaluaciones;
-
-    public String getNombre() {
-        return nombre;
+    public String getDirector() {
+        return director;
     }
 
-    public int getFechaLanzamiento() {
-        return fechaLanzamiento;
-    }
-
-    public int getDuracionEnMinuto() {
-        return duracionEnMinuto;
-    }
-
-    public boolean isIncluidoEnPlan() {
-        return incluidoEnPlan;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setFechaLanzamiento(int fechaLanzamiento) {
-        this.fechaLanzamiento = fechaLanzamiento;
-    }
-
-    public void setDuracionEnMinuto(int duracionEnMinuto) {
-        this.duracionEnMinuto = duracionEnMinuto;
-    }
-
-    public void setIncluidoEnPlan(boolean incluidoEnPlan) {
-        this.incluidoEnPlan = incluidoEnPlan;
-    }
-
-    public int getTotalEvaluaciones(){
-        return totalEvaluaciones;
-    }
-
-    public void nuestraFichaTecnica(){
-        System.out.println("Mi pelicula es: " + nombre);
-        System.out.println("Fecha de lanzamiento: " + fechaLanzamiento);
-        System.out.println("Duracion de la pelicula: " + duracionEnMinuto+ " min");
-    }
-
-    public void evalua(double nota){
-        totalEvaluaciones++;
-        sumaEvaluaciones+=nota;
-    }
-
-    public double calcularMedia(){
-        return sumaEvaluaciones/totalEvaluaciones;
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
---
+++ src/com/project1/screenmatch/modelos/Serie.java
@@ -0,0 +1,34 @@
+package com.project1.screenmatch.modelos;
+
+public class Serie extends Titulo{
+
+    int temporadas;
+
+    int episodiosPorTemporada;
+
+    int minutosPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+}
